cf40c685aaf61ef539af96af4ff52984442432ae
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-02-21 15:41:01 +0530

    Create FIRST CHARACTER.C
    to print the first character of the string

---
+++ FIRST CHARACTER.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+	char str[20];
+	int k,i;
+	printf("enter the string and the k value:");
+	scanf("%c %d",&str[i],&k);
+	for(i=0;i<k;i++)
+	{
+		printf("\n%c",str[i]);
+	}
+	return 0;
+}
